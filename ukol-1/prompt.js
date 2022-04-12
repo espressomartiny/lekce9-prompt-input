5377afad6bcd4bbd8cdc41911e3f3ab569413478
Martina Vokáčová <martinavokacova@gmail.com>
2022-04-12 20:50:13 +0200

    vysledek do html místo altertu, button, img

--- ukol-1/prompt.js
+++ ukol-1/prompt.js
@@ -2,26 +2,26 @@ const sirka = 62;
 const vyska = 70;
 const hloubka = 80;
 
-let sirkaPracky = prompt('Zadej šířku pračky v centimetrech');
-let vyskaPracky = prompt('Zadej výšku pračky v centimetrech');
-let hloubkaPracky = prompt('Zadej hloubku pračky v centimetrech');
+function zadejRozmery() {
+  let sirkaPracky = parseFloat(prompt("Zadej šířku pračky v centimetrech"));
+  let vyskaPracky = parseFloat(prompt("Zadej výšku pračky v centimetrech"));
+  let hloubkaPracky = parseFloat(prompt("Zadej hloubku pračky v centimetrech"));
 
-pomerPracku()
+  pomerPracku();
 
-
-function pomerPracku() {
-    if(sirka > sirkaPracky && vyska > vyskaPracky && hloubka > hloubkaPracky) {
-        alert("Vejde se ti tam, neboj.");
-        
+  function pomerPracku() {
+    if (sirka > sirkaPracky && vyska > vyskaPracky && hloubka > hloubkaPracky) {
+      document.querySelector(".vysledek").textContent =
+        "Vejde se ti tam, neboj.";
+    } else if (
+      sirka === sirkaPracky ||
+      vyska === vyskaPracky ||
+      hloubka === hloubkaPracky
+    ) {
+      document.querySelector(".vysledek").textContent = "Vleze se, ale těsně.";
+    } else {
+      document.querySelector(".vysledek").textContent = "Vopravdu se nevleze.";
+      document.querySelector(".tlacitko").textContent = "Porovnat jinou pračku";
     }
-    else if (sirka === sirkaPracky || vyska === vyskaPracky || hloubka === hloubkaPracky) {
-        alert("Vleze se, ale těsně.");
-        
-    }
-    else {
-        alert("Vopravdu se nevleze.");
-    }
-    
-  
+  }
 }
-
